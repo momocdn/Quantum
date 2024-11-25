73d58d02cd3b74278f1762aa1f77b6efdc4c0739
Deven Fahmideh-Norozy <dnorozy@gmail.com>
2024-11-25 12:48:59 -0500

    2x2 fini

--- nonOptimizedCaseTwo.py
+++ nonOptimizedCaseTwo.py
@@ -9,6 +9,8 @@ clause_list = [[0, 1],
 
 # Définir un appareil avec des noms de qubits
 named_wires = ["v0", "v1", "v2", "v3", "c0", "c1", "c2", "c3", "out0"]  # Noms des qubits
+sudoku_wires = ["v0", "v1", "v2", "v3"]
+aux0_wire = ["out0"]
 dev = qml.device("default.qubit", wires=named_wires)
 
 # Fonction XOR utilisant deux portes CNOT
@@ -29,15 +31,52 @@ def sudoku_oracle(clause_list):
     for i, clause in enumerate(clause_list):
         XOR(a=f"v{clause[0]}", b=f"v{clause[1]}", output=f"c{i}")  # Annule le XOR
 
-# Construire le circuit principal
-@qml.qnode(dev)
-def circuit():
+# Fonction qui construit le diffuseur de grover
+def sudoku_diffuser():
+
+    qml.broadcast(unitary=qml.Hadamard, pattern="single", wires=sudoku_wires)
+    qml.broadcast(unitary=qml.PauliX, pattern="single", wires=sudoku_wires)
+    qml.MultiControlledX(wires=sudoku_wires + aux0_wire, control_values=[1,1,1,1])
+    qml.broadcast(unitary=qml.PauliX, pattern="single", wires=sudoku_wires)
+    qml.broadcast(unitary=qml.Hadamard, pattern="single", wires=sudoku_wires)
+
+def iteration():
+    
     # Ajouter l'oracle Sudoku
     sudoku_oracle(clause_list)
 
+    # Ajouter diffuser grover
+    sudoku_diffuser()
+
+def initialisation():
+
+    # initialisation du dernier qubit pour le mettre à l'état |->
+    qml.PauliX(wires=aux0_wire)
+    qml.Hadamard(wires=aux0_wire)
+
+    # initialisation des qubits 'v' avec Hadamard
+    qml.broadcast(unitary=qml.Hadamard, pattern="single", wires=sudoku_wires)
+    
+
+# Construire le circuit principal
+@qml.qnode(dev)
+def circuit():
+
+    initialisation()
+   
+    # effectuer
+    for i in range(2):
+        iteration()
+        qml.Barrier()
+
     # Retourner l'état complet
-    return qml.state()
+    # return qml.state()
+
+    return qml.probs(wires=["v0", "v1", "v2", "v3"])
+
+print(circuit())
 
 # Dessiner le circuit
-drawer = qml.draw(circuit)
-print(drawer())
+# drawer = qml.draw(circuit)
+# print(drawer())
+
